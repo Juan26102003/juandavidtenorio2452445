1cf91ef06993b065435cd99250f72a379394ed80
unknown <jdtenorio63misena.edu.co>
2023-03-28 11:50:34 -0500

    practica

---
+++ autos/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class auto (models.Model):
+    marca=models.TextField(max_length=20) 
+    nombre=models.TextField(max_length=30)
+    modelo=models.IntegerField()
+    color=models.TextField(max_length=20)
+
+class clientes (models.Model):
+    nombre=models.TextField(max_length=20)
+    apellido=models.TextField(max_length=30)
+    direccion=models.TextField(max_length=200)
+    email=models.TextField(max_length=250)
+    telefono=models.TextField(max_length=20)
+
+
+# Create your models here.
